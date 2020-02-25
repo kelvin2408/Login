44c21a55bc76edd7c4d86e06f61b7dfe75ff71d0
kelvin2408 <53419032+kelvin2408@users.noreply.github.com>
2020-02-25 20:23:09 +0700

    Add files via upload

---
+++ src/views/Secure.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="secure">
+        <h1>Login Area</h1>
+        <p>
+            You're Logged In
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Secure',
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
